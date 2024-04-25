f6818b5abf5b45c1b13f0b9a733e82d95c1709fa
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-04-25 20:19:18 -0300

    feat: add Header component to pages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./style.css";
 export default function Header() {
   return (
-    <header>
-      <h1>headerr</h1>
+    <header className="container">
+      <Link to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
+
+      <Link className="reserva" to="/reservas">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>0 Reservas</span>
+        </div>
+      </Link>
     </header>
   );
 }
