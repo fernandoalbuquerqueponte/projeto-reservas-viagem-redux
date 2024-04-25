06948064741fdd1fb33d03521caee345017c9c19
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-04-25 19:51:05 -0300

    chore: add initial routes

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Reservas from "./pages/Reservas";
+
+export default function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservas" element={<Reservas />} />
+    </Routes>
+  );
+}
