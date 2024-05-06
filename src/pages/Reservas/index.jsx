73b91de58f6e4e76aba1822bb13028ee8e78f77e
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-05-06 17:24:47 -0300

    feat: add initial reserve page

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,7 +1,24 @@
+import "./style.css";
+import { MdDelete } from "react-icons/md";
 export default function Reservas() {
   return (
     <div>
-      <h1>Reservas</h1>
+      <h1 className="title">Você solicitou 1 reservas</h1>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="maceio"
+        />
+
+        <strong>Viagem maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
   );
 }
