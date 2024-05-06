9766046b679b846ec70ff3834552ab4092171b1b
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-05-06 15:57:29 -0300

    feat: add axios setup and fetch trips on Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import "./style.css";
+
+import { MdFlightTakeoff } from "react-icons/md";
+
 export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get("trips");
+      setTrips(response.data);
+      console.log(response.data);
+    }
+    loadApi();
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <div className="box">
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? "Disponivel" : "indisponivel"}</span>
+            <button type="button" onClick={() => {}}>
+              <MdFlightTakeoff size={16} color="#fff" />
+              Solicitar reserva
+            </button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
